3acea715085dd65d002a112813df24416ac61d1c
Mohamed Yasser <https://github.com/mohamedyasser37>
2026-06-25 15:51:06 +0300

    Update web view with medical history and social media9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "336535484" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2588375441" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
